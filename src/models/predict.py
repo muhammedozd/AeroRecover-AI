e64ce64c550c024ad24a25c971058b036bc9a4e6
Muhammed Özdemir <muhammedozdemir0@outlook.com>
2026-07-22 16:31:32 +0300

    Add model prediction and evaluation metrics

---
+++ src/models/predict.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+from xml.parsers.expat import model
+from src.data.preprocess_data import preprocess_data
+import joblib
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+
+
+# Projenin ana klasörünü bulur
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
+
+# Kaydedilmiş modelin yolu
+MODEL_PATH = PROJECT_ROOT / "models" / "logistic_regression.pkl"
+
+
+def load_model():
+    """Kaydedilmiş modeli diskten yükler."""
+    model = joblib.load(MODEL_PATH)
+    X_train, X_test, y_train, y_test = preprocess_data()
+    tahminler = model.predict(X_test)
+
+    accuracy = accuracy_score(y_test, tahminler)
+    print("İlk 5 tahmin:", tahminler[:5])
+    print(f"Accuracy: {accuracy:.2f}")
+
+    matrix = confusion_matrix(y_test, tahminler)
+
+    print("Confusion Matrix:")
+    print(matrix)
+
+    
+    print(f"Model başarıyla yüklendi: {MODEL_PATH}")
+    
+
+    return model
+
+
+if __name__ == "__main__":
+    load_model()
